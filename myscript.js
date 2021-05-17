50f5a4671a8643ce32e80b769d72bf57a82cfbf4
cafeman883 <sajukiller@gmail.com>
2021-05-17 10:11:56 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,13 +1,8 @@
-	var container = document.getElementById('map');
-	var options = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667),
-		level: 3
-	};
-
-	var map = new kakao.maps.Map(container, options);
-
 
 	function navi() {
+		
+		alert('test');
+		
     	Kakao.Navi.start({
       		name: '현대백화점 판교점',
             x: 127.11205203011632,
